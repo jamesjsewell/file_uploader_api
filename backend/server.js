983e5e0f73c7933176f736eaa3f772a843ebe82b
james sewell <jamesjosephsewell@gmail.com>
2018-06-11 10:34:00 -0500

    .

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,73 @@
 const express = require("express"),
-	bodyParser = require("body-parser")
-
+	bodyParser = require("body-parser"),
+	aws = require('aws-sdk'),
+    uppy = require('uppy-server'),
+    ms = require('ms'),
+    path = require('path')
 
 if(process.env.NODE_ENV == 'development'){
 	require('dotenv').config();
 }
 
-// connect to the database
-const connectToDB = require("./db_config.js").connectToDB
-connectToDB("test_project")
+const S3_BUCKET = process.env.S3_BUCKET,
+    S3_REGION = process.env.S3_REGION,
+    S3_KEY_ID = process.env.S3_KEY_ID,
+    S3_SECRET = process.env.S3_SECRET,
+    DATA_DIR = path.join(__dirname, 'tmp'),
+    SERVER_HOST = process.env.SERVER_HOST,
+    SERVER_PROTOCOL = process.env.SERVER_PROTOCOL,
+    UPPY_SECRET = process.env.UPPY_SECRET,
+    SHOULD_DEBUG = process.env.SHOULD_DEBUG
 
+console.log(S3_BUCKET, S3_REGION, S3_KEY_ID)
 // runs express app and sets defined port
 var app = express()
 const PORT = process.env.PORT || 3000 
-// app.set("port", PORT)
 
 // middleware, transforms http request so that you can use req.body json format 
 // for accepting json data from http requests
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
-// import sub routers for CRUD api
-let router = require("./api_features/router.js")
-
 app.all('*', function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-	res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
     next()
 });
 
-// base router for CRUD api
-app.use("/", router)
+const options = {
 
-// starts the app listening for requests
-app.listen(PORT, function () {
-	console.log(
-		"\n\n===== listening for requests on port " + PORT + " =====\n\n"
-	)
+    providerOptions: {
+    
+        s3: {
+
+            getKey: (req, filename) => filename,
+            key: S3_KEY_ID,
+            secret: S3_SECRET,
+            bucket: S3_BUCKET, 
+            region: S3_REGION//'us-east-1'
+
+        }
+
+    },
+    filePath: DATA_DIR,
+    debug: SHOULD_DEBUG === 'true' ? true : false,
+    secret: UPPY_SECRET,
+    server: {
+
+        host: SERVER_HOST, // or yourdomain.com
+        protocol: SERVER_PROTOCOL //"http"
+
+    }
+
+}
+
+app.use(uppy.app(options))
+
+const server = app.listen(3000, () => {
+    console.log('listening on port 3000')
 })
-
+  
+uppy.socket(server, options)
